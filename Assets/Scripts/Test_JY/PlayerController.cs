5f6b997e6c3bc611c431105f33f1a93b585fee2b
Princess._.Yeol <gongjunyeol@gmail.com>
2023-03-15 21:41:44 +0900

    Light Jump / Full Jump & Dashing
    라이트 점프, 풀 점프 구현
    대쉬 기능 구현
    
    # 버그 발견 #
    플랫폼 아래서 껴가지고 점프하면 점프가 막힌다 (_hasJumped 안풀림 버그)

--- Assets/Scripts/Test_JY/PlayerController.cs
+++ Assets/Scripts/Test_JY/PlayerController.cs
@@ -1,12 +1,15 @@
 using System;
 using Unity.VisualScripting.Antlr3.Runtime;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     private Inputs _inputs;
-    //private bool _userGravity;
+    private bool _useGravity;
     
     void Update()
     {
@@ -14,6 +17,7 @@ public class PlayerController : MonoBehaviour
         HandleGrounding();
         HandleWalking();
         HandleJumping();
+        HandleDashing();
     }
 
     #region Inputs
@@ -27,7 +31,8 @@ public class PlayerController : MonoBehaviour
         _inputs.X = Input.GetAxis("Horizontal");
         _inputs.Y = Input.GetAxis("Vertical");
 
-        _facingLeft = _inputs.RawX != 1 && (_inputs.RawX == -1 || _facingLeft);
+        _facingLeft = _inputs.RawX == -1 || _facingLeft;
+            // _inputs.RawX != 1 && (_inputs.RawX == -1 || _facingLeft);
     }
 
     #endregion
@@ -37,7 +42,7 @@ public class PlayerController : MonoBehaviour
     [Header("Detection")] [SerializeField] private LayerMask _groundMask;
     [SerializeField] private float _grounderOffset = -1, _grounderRadius = 0.2f;
     public bool IsGrounded;
-    public static event Action OnTouchedGround;
+    //public static event Action OnTouchedGround;
 
     private readonly Collider2D[] _ground = new Collider2D[1];
 
@@ -48,17 +53,18 @@ public class PlayerController : MonoBehaviour
             _grounderRadius, _ground, _groundMask) > 0;
 
         // OnGrounded
-        if (!IsGrounded && grounded)
+        if (!IsGrounded && grounded) // land
         {
             IsGrounded = true;
             _hasJumped = false;
             _enableDoubleJump = true;
+            _hasDoubleJumped = false;
             _currentMovementLerpSpeed = 100;
-            OnTouchedGround?.Invoke();
+            //OnTouchedGround?.Invoke();
 
         }
         // OffGrounded
-        else if (IsGrounded && !grounded)
+        else if (IsGrounded && !grounded) // jump timing
         {
             IsGrounded = false;
             _timeLeftGrounded = Time.time;
@@ -96,6 +102,7 @@ public class PlayerController : MonoBehaviour
                 _inputs.X = 0;
             }
 
+            // Smooth
             _inputs.X = Mathf.MoveTowards(_inputs.X, -1, acceleration * Time.deltaTime);
         }
         // right
@@ -106,6 +113,7 @@ public class PlayerController : MonoBehaviour
                 _inputs.X = 0;
             }
 
+            // Smooth
             _inputs.X = Mathf.MoveTowards(_inputs.X, 1, acceleration * Time.deltaTime);
         }
         // none
@@ -118,23 +126,22 @@ public class PlayerController : MonoBehaviour
 
         // _currentMovementLerpSpeed should be set to something crazy high to be effectively instant. But slowed down after a wall jump and slowly released
         _rb.velocity = Vector3.MoveTowards(_rb.velocity, idealVel, _currentMovementLerpSpeed * Time.deltaTime);
-
     }
-
+    
     #endregion
 
     #region Jumping
 
-    [Header("Jumping")] [SerializeField] private float _jumpForce = 25;
+    [Header("Jumping")] [SerializeField] private float _jumpForce = 12;
     [SerializeField] private float _fallMultiplier = 7;
     [SerializeField] private float _jumpVelocityFalloff = 8;
     //[SerializeField] private Transform _jumpLaunchPoof;
     [SerializeField] private float _coyoteTime = 0.2f;
+    [SerializeField] private bool _hasJumped;
     [SerializeField] private bool _enableDoubleJump = true;
+    [SerializeField] private bool _hasDoubleJumped;
 
     private float _timeLeftGrounded = -10;
-    private bool _hasJumped;
-    private bool _hasDoubleJumped;
 
     private void HandleJumping()
     {
@@ -143,12 +150,15 @@ public class PlayerController : MonoBehaviour
             if (IsGrounded || (Time.time < _timeLeftGrounded + _coyoteTime) || (_enableDoubleJump && !_hasDoubleJumped))
             {
                 if (!_hasJumped || (_hasJumped && !_hasDoubleJumped)){
-                    ExecuteJump(new Vector2(_rb.velocity.x, _jumpForce), _hasJumped); // Ground jump
+                    ExecuteJump(new Vector2(_rb.velocity.x, _jumpForce), _hasJumped); // Ground jump (x에 _rb.velocity.x를 줌으로써 더 멀리 점프 가능)
+                    // _hasJumped가 false일 때 들어왔다? -> 1단 점프가 실행된다는 뜻
+                    // _hasJumped가 true일 때 들어왔다? -> 2단 점프가 실행된다는 뜻
                 }
             }
         }
 
-        void ExecuteJump(Vector3 dir, bool doubleJump = false)
+        // execute jump function
+        void ExecuteJump(Vector2 dir, bool doubleJump = false)
         {
             _rb.velocity = dir;
             _hasDoubleJumped = doubleJump;
@@ -158,9 +168,65 @@ public class PlayerController : MonoBehaviour
         }
 
         // Fall faster and allow small jumps. _jumpVelocityFalloff is the point at which we start adding extra gravity. Using 0 causes floating
-        if ((_rb.velocity.y < _jumpVelocityFalloff) || ((_rb.velocity.y > 0) && !Input.GetKey(KeyCode.C)))
+        // Light Jump & Full Jump
+        if ((_rb.velocity.y < _jumpVelocityFalloff) || ((_rb.velocity.y > 0) && !Input.GetKey(KeyCode.Space)))
+        {
             _rb.velocity += _fallMultiplier * Physics2D.gravity.y * Vector2.up * Time.deltaTime;
+        }
     }
+
+    #endregion
+
+    #region Dashing
+
+    [Header("Dash")]
+    [SerializeField] private float _dashSpeed = 35.0f;
+    [SerializeField] private float _dashLength = 0.22f;
+    [SerializeField] private bool _hasDashed;
+    [SerializeField] private bool _dashing;
+
+    //public static event Action OnStartDashing, OnStopDashing;
+
+    private float _timeStartedDash;
+    private Vector2 _dashDir;
+
+    private void HandleDashing()
+    {
+        // Dash
+        if(Input.GetKeyDown(KeyCode.X) && !_hasDashed)
+        {
+            _dashDir = new Vector2(_inputs.RawX, _inputs.RawY).normalized; // 대쉬 방향
+            if (_dashDir == Vector2.zero)
+                _dashDir = _facingLeft ? Vector2.left : Vector2.right;
+            _dashing = true;
+            _hasDashed = true;
+            _timeStartedDash = Time.time;
+            _useGravity = false;
+            _rb.gravityScale = _useGravity ? 1 : 0;
+            //OnStartDashing?.Invoke();
+        }
+
+        // Already Dash
+        if(_dashing)
+        {
+            _rb.velocity = _dashDir * _dashSpeed;
+
+            if(Time.time >= _timeStartedDash + _dashLength)
+            {
+                _dashing = false;
+                // Clamp the velocity so they don't keep shooting off
+                _rb.velocity = new Vector2(_rb.velocity.x, (_rb.velocity.y > 3) ? 3 : _rb.velocity.y);
+                _useGravity = true;
+                _rb.gravityScale = _useGravity ? 1 : 0;
+                _hasDashed = false;
+                //OnStopDashing?.Invoke();
+            }
+        }
+    }
+
+
+
+
 
     #endregion
 
