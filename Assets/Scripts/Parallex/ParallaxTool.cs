0ddb098b4695b4f6bc1b8a60588cfb5b382e59d6
HappyFaceFriend <yjl980730@gmail.com>
2023-03-17 02:09:30 +0900

    Parallax Tool 툴팁 추가

--- Assets/Scripts/Parallex/ParallaxTool.cs
+++ Assets/Scripts/Parallex/ParallaxTool.cs
@@ -10,15 +10,23 @@ using UnityEditor;
 public class ParallaxTool : MonoBehaviour
 {
     public enum VolumetricLightType { DistanceBased, LayerBased}
+    [Tooltip("각 레이어 별 최소 z 값을 지정합니다. \n마지막 레이어는 이전 레이어의 최소 z값보다 작은 모든 이미지를 포함합니다.")]
     [SerializeField] ParallaxBoundaries _parallaxBoundaries= new ParallaxBoundaries();
+
     [Header("Volumetric Light")]
-    [SerializeField] Color _volumetricLightColor = Color.white;
-    [SerializeField][Range(0f, 1f)] float _volumeOpacity = 1f;
-    [SerializeField]float _volumeDepth = 200f;
+    [SerializeField][Tooltip("안개 효과 색")] Color _volumetricLightColor = Color.white;
+    [SerializeField][Tooltip("안개 효과 강도")][Range(0f, 1f)] float _volumeOpacity = 1f;
+
+    [Tooltip("안개효과 적용 방식\n DistanceBased : 거리를 기준으로 투명도 설정\n LayerBased : 같은 안개 이미지를 겹겹이 쌓아서 생성")]
     [SerializeField] VolumetricLightType _volumeType = VolumetricLightType.DistanceBased;
 
+    [Tooltip("Distance Based 방식 사용 시 월드의 최대 깊이")]
+    [SerializeField]float _volumeDepth = 200f;
+
     [Header("Global Light")]
+    [Tooltip("월드 전체에 적용 될 빛의 색")]
     [SerializeField] Color _globalLightColor = Color.white;
+    [Tooltip("월드 전체에 적용 될 빛의 세기")]
     [SerializeField] float _globalLightIntensity = 1f;
 
     SpriteRenderer[] _volumetricLights;
@@ -82,10 +90,12 @@ public class ParallaxTool : MonoBehaviour
                     _volumetricLights[j].color = new Color(_volumetricLights[j].color.r, _volumetricLights[j].color.g, _volumetricLights[j].color.b,
                                                     _volumetricLights[j].color.a - color.a);
             }
-            _volumetricLights[i].transform.position = new Vector3(_volumetricLights[i].transform.position.x, _volumetricLights[i].transform.position.y, z);
+            //TODO : 안개효과 이미지가 카메라를 쫓아가고 사이즈가 카메라에 딱맞게 만들기? 를 해야할지도
+            _volumetricLights[i].transform.position = new Vector3(0, 0, z);
             _volumetricLights[i].color = color;
             _volumetricLights[i].sortingLayerName = _parallaxBoundaries.GetEnabledLayerNames()[i];
             _volumetricLights[i].sortingOrder = GetSortingOrder(minBoundaries[i - 1]);
+
         }
         if (_volumeType == VolumetricLightType.DistanceBased)
             Camera.main.backgroundColor = _volumetricLightColor * _volumeOpacity;
@@ -105,7 +115,7 @@ public class ParallaxToolInspector: Editor
         base.OnInspectorGUI();
 
         ParallaxTool tool = (ParallaxTool)target;
-        if (GUILayout.Button("Sort Layers"))
+        if (GUILayout.Button(new GUIContent("Sort Layers", "Parallax Boundaries 설정에 맞게 모든 이미지를 정렬")))
         {
             tool.SortLayers();
         }
