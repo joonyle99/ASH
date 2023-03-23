c77e4756fa6024da083160dc3ccb189480d26545
HappyFaceFriend <yjl980730@gmail.com>
2023-03-24 03:55:22 +0900

    망토 완전 기초 프로토타입 구현

---
+++ Assets/Scripts/Test/Prototype/CapeScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CapeScript : MonoBehaviour
+{
+    public Sprite capeSrite;
+
+    public Mesh SpriteToMesh(Sprite sprite)
+    {
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(System.Array.ConvertAll(sprite.vertices, i => (Vector3)i).ToList());
+        mesh.SetUVs(0, sprite.uv.ToList());
+        mesh.SetTriangles(System.Array.ConvertAll(sprite.triangles, i => (int)i), 0);
+
+        return mesh;
+    }
+
+}
+[CustomEditor(typeof(CapeScript), true)]
+public class CapeScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        CapeScript t = (CapeScript)target;
+        if(GUILayout.Button("Make mesh"))
+        {
+            Mesh mesh = t.SpriteToMesh(t.capeSrite);
+            AssetDatabase.CreateAsset(mesh, "Assets/mesh.asset");
+            AssetDatabase.SaveAssets();
+        }
+        DrawDefaultInspector();
+        Repaint();
+    }
+}
