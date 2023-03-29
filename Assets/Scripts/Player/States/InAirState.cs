7c96ee1a5b2d11eecb67b6b637688a92cd941ae2
HappyFaceFriend <yjl980730@gmail.com>
2023-03-30 05:54:56 +0900

    Fast drop 기능 추가

--- Assets/Scripts/Player/States/InAirState.cs
+++ Assets/Scripts/Player/States/InAirState.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class InAirState : PlayerState
 {
     [SerializeField] float _moveSpeed = 7f;
+    [SerializeField] float _fastDropThreshhold = 7f;
+    [SerializeField] float _fastDropPower = 7f;
 
     PlayerJumpController _jumpController;
     protected override void OnEnter()
@@ -15,9 +17,16 @@ public class InAirState : PlayerState
 
     protected override void OnUpdate()
     {
+        //좌우 입력
         float xInput = Player.SmoothedInputs.Movement.x;
         Vector3 targetVelocity = new Vector3(xInput * _moveSpeed, Player.Rigidbody.velocity.y);
         Player.Rigidbody.velocity = targetVelocity;
+
+        //떨어지는 속도 증가
+        if (Player.Rigidbody.velocity.y < 0)
+        {
+            Player.Rigidbody.velocity += _fastDropPower * Physics2D.gravity * Time.deltaTime;
+        }
 
         if (Player.IsGrounded)
         {
