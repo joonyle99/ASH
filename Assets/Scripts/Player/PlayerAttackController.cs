af14e9b3c29b152fa8bed792a63a31585de11806
Princess._.Yeol <67359781+JUNYEOL-GONG@users.noreply.github.com>
2023-06-04 21:15:10 +0900

    플레이어 파이어볼 구현중

--- Assets/Scripts/Player/PlayerAttackController.cs
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -9,15 +9,21 @@ public class PlayerAttackController : MonoBehaviour
     int _basicAttackCount = 0 ;
 
     [SerializeField] float _attackCountRefreshTime;
+    [SerializeField] float _shootingCoolTime;
     [SerializeField] Transform _basicAttackHitbox;
+    [SerializeField] GameObject _fireBullet;
+
     float _timeAfterLastBasicAttack;
+    float _timeAfterLastShootingAttack;
     public bool IsBasicAttacking { get; private set; }
+    public bool IsShootingAttacking { get; private set; }
 
     [SerializeField] Animator _animator;
     private void Awake()
     {
         _playerBehaviour = GetComponent<PlayerBehaviour>();
     }
+
     public void CastBasicAttack()
     {
         _basicAttackHitbox.gameObject.SetActive(true);
@@ -33,9 +39,16 @@ public class PlayerAttackController : MonoBehaviour
         IsBasicAttacking = true;
     }
 
+    public void CastShootingAttack()
+    {
+        if (!IsShootingAttacking)
+        {
+            StartCoroutine(ShootingFire());
+        }
+    }
+
     private void Update()
     {
-
         if (_timeAfterLastBasicAttack < _attackCountRefreshTime)
         {
             _timeAfterLastBasicAttack += Time.deltaTime;
@@ -50,4 +63,25 @@ public class PlayerAttackController : MonoBehaviour
         _basicAttackHitbox.gameObject.SetActive(false);
     }
 
+    private IEnumerator ShootingFire()
+    {
+        IsShootingAttacking = true;
+
+        // 0.8초의 딜레이
+        // 이 동안은 움직일 수 없어야 한다.
+        yield return new WaitForSeconds(0.8f);
+
+        _basicAttackHitbox.gameObject.SetActive(true);
+
+        //_timeAfterLastBasicAttack = 0f;
+        //_animator.SetInteger("BasicAttackCount", _basicAttackCount);
+        //_animator.SetTrigger("BasicAttack");
+
+        GameObject bullet = Instantiate(_fireBullet, _basicAttackHitbox.transform.position + new Vector3(0.4f * _playerBehaviour.RecentDir, 0.8f), transform.rotation);
+        bullet.transform.localScale *= _playerBehaviour.RecentDir;
+
+        IsShootingAttacking = false;
+
+        yield return 0;
+    }
 }
