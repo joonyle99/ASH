1b561c5736aeb6a0b63ba549e94660f1fa5e4ebc
Princess._.Yeol <67359781+JUNYEOL-GONG@users.noreply.github.com>
2023-07-16 21:58:14 +0900

    플레이어 내려찍기, 파이어볼 작업 중
    ******************
    플레이어 스크립트 정리해야한다.
    상태, 컨트롤러 정리

--- Assets/Scripts/Player/States/DesolateDiveState.cs
+++ Assets/Scripts/Player/States/DesolateDiveState.cs
@@ -15,7 +15,9 @@ public class DesolateDiveState : PlayerState
     [SerializeField] float _explosionSizeY = 1.0f;      // 폭발 높이
     [SerializeField] int _explosionDamage = 40;         // 폭발 데미지
     [SerializeField] float _knockBackPower = 10f;       // 넉백 파워
-    [SerializeField] bool _isDiving = false;            // 다이빙 상태 (차징은 다이빙 X)
+
+    [SerializeField] bool _isCharging = false;          // 차징 상태
+    [SerializeField] bool _isDiving = false;            // 다이빙 상태
 
     protected override void OnEnter()
     {
@@ -25,10 +27,13 @@ public class DesolateDiveState : PlayerState
 
     protected override void OnUpdate()
     {
+        Player.Animator.SetBool("IsCharging", _isCharging);
+        Player.Animator.SetBool("IsDiving", _isDiving);
+
         if (!_isDiving)
             return;
 
-        // 가속도 적용
+        // 가속도 적용1
         Player.Rigidbody.velocity -= Vector2.down * _fastDiveSpeed * Physics2D.gravity.y * Time.deltaTime;
 
         // 내려찍기가 끝나면 => 데미지 & 넉백
@@ -66,13 +71,16 @@ public class DesolateDiveState : PlayerState
     {
         // Start Charging
 
+        _isCharging = true;
+
+        // 내려찍기 애니메이션 시작점
+        // Player.Animator.SetBool("Dive", true);
+
+        // 공중에 떠있는 상태로 고정
         Player.Rigidbody.gravityScale = 0;
         Player.Rigidbody.velocity = Vector2.zero;
 
-        // Charging Animation
-        Player.Animator.SetTrigger("Dive");
-
-        // create particle system
+        // 차징 파티클 생성
         ParticleSystem effect = Instantiate(_chargingParticle, transform.position + new Vector3(0f, 0.5f), Quaternion.identity, transform);
         effect.Play();
 
@@ -80,20 +88,26 @@ public class DesolateDiveState : PlayerState
 
         // End Charging / Start Dive
 
-        // delete particle system
+        // 차징 파티클 종료
         effect.Stop();
         Destroy(effect.gameObject);
 
-        _isDiving = true;
-
-        // init Rigidbody
+        // 내려찍기를 위한 Rigidbody 설정
         Player.Rigidbody.gravityScale = 5;
         Player.Rigidbody.velocity = new Vector2(0, -_diveSpeed);
+
+        _isCharging = false;
+        _isDiving = true;
     }
 
     protected override void OnExit()
     {
         //Debug.Log("Exit Desolate Dive");
+
+        // Charging Animation
+        // Player.Animator.SetBool("Dive", false);
+
+        _isDiving = false;
     }
 
     void OnDrawGizmosSelected()
--- Assets/Scripts/Player/States/ShootingState.cs
+++ Assets/Scripts/Player/States/ShootingState.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// ReSharper disable All
 
 public class ShootingState : PlayerState
 {
