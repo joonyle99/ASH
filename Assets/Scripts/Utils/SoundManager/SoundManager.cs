910b091eb44e12d92945965aad4c27019333263d
HappyFaceFriend <yjl980730@gmail.com>
2023-07-18 11:47:44 +0900

    SoundManager에 common soundlist 지원

--- Assets/Scripts/Utils/SoundManager/SoundManager.cs
+++ Assets/Scripts/Utils/SoundManager/SoundManager.cs
@@ -7,9 +7,24 @@ public class SoundManager : HappyTools.SingletonBehaviour<SoundManager>
 {
     //Dictionary<float, AudioSource> _audioSources;
 
+    [SerializeField] GameObject _soundListParent;
+
     [SerializeField] AudioSource _sfxPlayer;
     [SerializeField] AudioSource _bgmPlayer;
 
+    SoundList [] _soundLists;
+    Dictionary<string, int> _soundListIndicies  = new Dictionary<string, int>();
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _soundLists = _soundListParent.GetComponentsInChildren<SoundList>();
+        for(int i = 0; i< _soundLists.Length; i++)
+        {
+            for (int j = 0; j < _soundLists[i].Datas.Count; j++)
+                _soundListIndicies[_soundLists[i].Datas[j].Key] = i;
+        }
+    }
     public void PlaySFXPitched(AudioClip clip, float pitchMultiplier = 1, float volumeMultiplier = 1f)
     {
         _sfxPlayer.PlayOneShot(clip, volumeMultiplier);
@@ -18,4 +33,28 @@ public class SoundManager : HappyTools.SingletonBehaviour<SoundManager>
     {
         _bgmPlayer.PlayOneShot(clip, volumeMultiplier);
     }
+
+    public void PlayCommonSFXPitched(string key, float pitchMultiplier = 1, float volumeMultiplier = 1f)
+    {
+        if (_soundListIndicies.ContainsKey(key))
+        {
+            _soundLists[_soundListIndicies[key]].PlaySFXPitched(key, pitchMultiplier, volumeMultiplier);
+        }
+        else
+        {
+            Debug.LogWarning("No SFX matching: " + key);
+        }
+    }
+    public void PlayCommonBGM(string key, float volumeMultiplier = 1f)
+    {
+        if (_soundListIndicies.ContainsKey(key))
+        {
+            _soundLists[_soundListIndicies[key]].PlaySFXPitched(key, volumeMultiplier);
+        }
+        else
+        {
+            Debug.LogWarning("No BGM matching: " + key);
+        }
+    }
+
 }
