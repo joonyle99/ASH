c415c5a921592bd5bca0ce9683172f0e055bd5da
HappyFaceFriend <yjl980730@gmail.com>
2023-07-25 14:23:27 +0900

    NPC 2종 추가

--- Assets/Scripts/UIs/Interactions/InteractionMarker.cs
+++ Assets/Scripts/UIs/Interactions/InteractionMarker.cs
@@ -7,6 +7,7 @@ public class InteractionMarker : MonoBehaviour
 {
     RectTransform _rectTransform;
 
+    InteractableObject _currentInteractable;
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>(); 
@@ -14,7 +15,12 @@ public class InteractionMarker : MonoBehaviour
     public void EnableAt(InteractableObject interactableObject)
     {
         gameObject.SetActive(true);
+        _currentInteractable = interactableObject;
         _rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(interactableObject.InteractionMarkerPoint) - new Vector3(Screen.width/2, Screen.height/2);
+    }
+    public void LateUpdate()
+    {
+        _rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(_currentInteractable.InteractionMarkerPoint) - new Vector3(Screen.width / 2, Screen.height / 2);
     }
     public void Disable()
     {
