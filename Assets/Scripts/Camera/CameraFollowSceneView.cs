9ecf923022cad0090d112ace9197977edf0ba7b7
HappyFaceFriend <yjl980730@gmail.com>
2023-07-27 01:33:04 +0900

    로그 제거

--- Assets/Scripts/Camera/CameraFollowSceneView.cs
+++ Assets/Scripts/Camera/CameraFollowSceneView.cs
@@ -9,7 +9,6 @@ public class CameraFollowSceneView : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(SceneView.lastActiveSceneView);
         Vector3 sceneViewPos = SceneView.lastActiveSceneView.camera.transform.position;
         sceneViewPos.z = transform.position.z;
         transform.position = sceneViewPos;
