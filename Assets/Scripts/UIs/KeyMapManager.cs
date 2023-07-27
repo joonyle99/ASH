14eebc9f17e20d51c45444f22c61b1f8b197bc1e
HappyFaceFriend <yjl980730@gmail.com>
2023-07-28 02:40:41 +0900

    키맵 변경

--- Assets/Scripts/UIs/KeyMapManager.cs
+++ Assets/Scripts/UIs/KeyMapManager.cs
@@ -1,3 +1,4 @@
+using Com.LuisPedroFonseca.ProCamera2D.TopDownShooter;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,31 @@ public class KeyMapManager : MonoBehaviour
 {
     [SerializeField] GameObject _keyMapUI;
     [SerializeField] KeyCode _showKey;
+    [SerializeField] float _firstDuration = 10f;
+
+    static bool _isFirst = true;
+
+    private void Start()
+    {
+        if (_isFirst)
+        {
+            StartCoroutine(RemoveKeymap());
+        }
+    }
+    IEnumerator RemoveKeymap()
+
+    {
+        yield return new WaitForSeconds(_firstDuration);
+        _keyMapUI.GetComponent<Animator>().SetTrigger("Close");
+        yield return new WaitForSeconds(0.5f);
+        _keyMapUI.SetActive(false);
+    }
     public void Update()
     {
         if (Input.GetKeyDown(_showKey))
         {
             _keyMapUI.SetActive(!_keyMapUI.activeInHierarchy);
+            _isFirst = false;
         }
     }
 }
