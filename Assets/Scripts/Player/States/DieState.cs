6e97f56f04f33282354742d05938853d47ee2480
Princess._.Yeol <67359781+JUNYEOL-GONG@users.noreply.github.com>
2023-07-28 01:29:28 +0900

    플레이어 DieState 버그 수정

--- Assets/Scripts/Player/States/DieState.cs
+++ Assets/Scripts/Player/States/DieState.cs
@@ -6,47 +6,27 @@ public class DieState : PlayerState
     public Transform respawnPoint;
     [Range(0f, 10f)] public float disapearTime;
     [Range(0f, 10f)] public float reviveDelay;
+    private SpriteRenderer[] renderers;
+    private float[] startAlphas;
+    private float time = 0f;
 
     protected override void OnEnter()
     {
-        StartCoroutine(Die());
+        time = 0f;
+        Animator.SetBool("IsDead", true);
+        this.GetComponent<Collider2D>().enabled = false;
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+        startAlphas = new float[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++)
+            startAlphas[i] = renderers[i].color.a;
     }
 
     protected override void OnUpdate()
     {
-
-    }
-
-    protected override void OnExit()
-    {
-
-    }
-
-    /// <summary>
-    /// 플레이어가 죽는 함수
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator Die()
-    {
-        Animator.SetBool("IsDead", true);
-
-        // 콜라이더 비활성화
-        this.GetComponent<Collider2D>().enabled = false;
-
-        // 자식 오브젝트의 모든 렌더 컴포넌트를 가져온다
-        SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer>();
-
-        // 초기 알파값 저장
-        float[] startAlphas = new float[renderers.Length];
-        for (int i = 0; i < renderers.Length; i++)
-            startAlphas[i] = renderers[i].color.a;
-
-        // 모든 렌더 컴포넌트를 돌면서 Fade Out
-        float t = 0;
-        while (t < disapearTime)
+        if (time < disapearTime)
         {
-            t += Time.deltaTime;
-            float normalizedTime = t / 2;
+            time += Time.deltaTime;
+            float normalizedTime = time / 2;
 
             for (int i = 0; i < renderers.Length; i++)
             {
@@ -54,17 +34,17 @@ public class DieState : PlayerState
                 color.a = Mathf.Lerp(startAlphas[i], 0f, normalizedTime);
                 renderers[i].color = color;
             }
-
-            yield return null;
         }
+        else
+        {
+            gameObject.SetActive(false);
+            SceneManager.Instance.ReactivatePlayerAfterDelay(respawnPoint.position, reviveDelay);
+        }
+    }
 
+    protected override void OnExit()
+    {
         Animator.SetBool("IsDead", false);
-
-        // 오브젝트의 사망 처리
-        gameObject.SetActive(false);
-        SceneManager.Instance.ReactivatePlayerAfterDelay(respawnPoint.position, reviveDelay);
-
-        yield return null;
     }
 }
 
