fc8f9029a05dd6ef7feb6ae835bedd5ff5efc2f4
HappyFaceFriend <yjl980730@gmail.com>
2023-10-17 06:14:55 +0900

    부술 수 있는 돌(소형돌) 추가

--- Assets/Scripts/MapObjects/CrashableStone.cs
+++ Assets/Scripts/MapObjects/CrashableStone.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrashableRock : AttackableEntity
+public class CrashableStone : AttackableEntity
 {
     [SerializeField] List<ParticleHelper> _particles;
-    [SerializeField] List<Transform> _pieces;
+    [SerializeField] List<Transform> _soundPieces;
     override protected void OnHittedByBasicAttack(PlayerBehaviour player)
     {
         SoundManager.Instance.PlayCommonSFXPitched("SE_CrashRock_hit");
@@ -18,16 +18,16 @@ public class CrashableRock : AttackableEntity
             particle.Activate();
             particle.transform.parent = null;
             particle.transform.localScale = Vector3.one;
+            particle.transform.rotation = Quaternion.identity;
         }
-
-        GetComponent<Collider2D>().enabled = false;
-
-        foreach (var piece in _pieces)
+        foreach (var piece in _soundPieces)
         {
-            piece.transform.parent = transform.parent;
+            piece.transform.parent = null;
             piece.gameObject.SetActive(true);
         }
-        Destroy(gameObject);
+
+        Destroy(transform.parent.gameObject);
     }
+
 
 }
