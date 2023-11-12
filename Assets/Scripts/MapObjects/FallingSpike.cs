c489ccbe93f238c025398bd7847e6e19e4f216c8
HappyFaceFriend <yjl980730@gmail.com>
2023-11-12 21:58:36 +0900

    떨어지는 가시 raycast 방식으로 변경

--- Assets/Scripts/MapObjects/FallingSpike.cs
+++ Assets/Scripts/MapObjects/FallingSpike.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FallingSpike : MonoBehaviour, ITriggerListener, IAttackListener
 {
     Rigidbody2D _rigidbody;
-
+    [SerializeField] float _raycastWidth = 0.5f;
     [SerializeField] float _fallDelay = 0.3f;
     [SerializeField] float _fallSpeedBonus = 0f;
+    [SerializeField] LayerMask _collisionLayers;
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -17,7 +18,7 @@ public class FallingSpike : MonoBehaviour, ITriggerListener, IAttackListener
         if (isBasicAttack)
             Destroy(gameObject);
     }
-
+    /*
     public void OnEnterReported(TriggerActivator activator, TriggerReporter reporter)
     {
         if (activator.Type == ActivatorType.Player)
@@ -25,10 +26,32 @@ public class FallingSpike : MonoBehaviour, ITriggerListener, IAttackListener
             Invoke("Fall", _fallDelay);
             Destroy(reporter.gameObject);
         }
+    }*/
+
+    void FixedUpdate()
+    {
+        {
+            var hit = Physics2D.Raycast(transform.position + new Vector3(_raycastWidth / 2, 0, 0), Vector2.down, 100, _collisionLayers);
+            if (hit && hit.transform.GetComponent<PlayerBehaviour>())
+            {
+                Invoke("Fall", _fallDelay);
+                return;
+            }
+        }
+        {
+            var hit = Physics2D.Raycast(transform.position - new Vector3(_raycastWidth / 2, 0, 0), Vector2.down, 100, _collisionLayers);
+            if (hit && hit.transform.GetComponent<PlayerBehaviour>())
+            {
+                Invoke("Fall", _fallDelay);
+                return;
+            }
+        }
+
     }
     void Fall()
     {
         _rigidbody.AddForce(new Vector2(0, _fallSpeedBonus), ForceMode2D.Impulse);
         _rigidbody.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
     }
+
 }
