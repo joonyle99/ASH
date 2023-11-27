703d7877a6feb9365329b9d43760825ca83d5d4a
HappyFaceFriend <yjl980730@gmail.com>
2023-11-28 02:39:51 +0900

    숨겨진 길 편집하기 편하게 수정

--- Assets/Scripts/MapObjects/HiddenPath/HiddenPath.cs
+++ Assets/Scripts/MapObjects/HiddenPath/HiddenPath.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HiddenPath : MonoBehaviour, ILightCaptureListener
+public class HiddenPath : MonoBehaviour, ILightCaptureListener, ITriggerListener
 {
     [SerializeField] HiddenPathMask _mask;
-    [SerializeField] GameObject _coveringWallParent;
+    [SerializeField] HiddenPathMask.Direction _swipeDirection;
+    [SerializeField] float _swipeDuration;
+    [SerializeField] Collider2D [] _disablingColliders;
 
+    LayerMask [] _originalColliderLayers;
+    void Awake()
+    {
+        _mask.InitMask(_swipeDirection);
+    }
     public void OnLightCaptured(LightCapturer capturer, LightSource lightSource)
     {
-        _mask.OnLightCaptured();
+        _mask.OnLightCaptured(_swipeDuration);
         Destroy(capturer.gameObject);
-        Destroy(_coveringWallParent);
+    }
+
+    public void OnEnterReported(TriggerActivator activator, TriggerReporter reporter)
+    { 
+        if (activator.Type == ActivatorType.Player)
+        {
+            _originalColliderLayers = new LayerMask[_disablingColliders.Length];
+            for (int i=0; i< _disablingColliders.Length; i++)
+            {
+                _originalColliderLayers[i] = _disablingColliders[i].gameObject.layer;
+                _disablingColliders[i].gameObject.layer = LayerMask.NameToLayer("ExceptPlayer");
+            }
+        }
+    }
+    public void OnExitReported(TriggerActivator activator, TriggerReporter reporter)
+    {
+        if (activator.Type == ActivatorType.Player)
+        {
+            for (int i = 0; i < _disablingColliders.Length; i++)
+            {
+                _disablingColliders[i].gameObject.layer = _originalColliderLayers[i];
+            }
+        }
     }
 }
