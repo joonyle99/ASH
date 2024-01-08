e7da89958ae7a490b2910a6613ddc30c59a77f22
joonyle99 <gongjunyeol@gmail.com>
2024-01-08 22:24:29 +0900

    몬스터 애니메이션 수정

--- Assets/Scripts/Monster/_Module/GroundChaseEvaluator.cs
+++ Assets/Scripts/Monster/_Module/GroundChaseEvaluator.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class GroundChaseEvaluator : MonoBehaviour
 {
-    [Header("Ground Chase Evaluator")]
-    [Space]
 
-    [SerializeField] private bool isGood = false;
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-    }
 }
