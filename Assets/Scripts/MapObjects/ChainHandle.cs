d967f6e46e1edd1149a51d4f7fe4b0a2b04bce7f
HappyFaceFriend <yjl980730@gmail.com>
2024-01-09 19:15:38 +0900

    체인이 늘어나는 버그 해결

--- Assets/Scripts/MapObjects/ChainHandle.cs
+++ Assets/Scripts/MapObjects/ChainHandle.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class ChainHandle : MonoBehaviour
 {
     [SerializeField] Joint2D _jointWithPlayer;
+    Rigidbody2D _rigidbody;
+    SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     public void ConnectTo(Rigidbody2D target)
     {
+        _spriteRenderer.enabled = false;
+        _rigidbody.freezeRotation = true;
         _jointWithPlayer.connectedBody = target;
         _jointWithPlayer.enabled = true;
     }
     public void Disconnect()
     {
+        _spriteRenderer.enabled = true;
+        _rigidbody.freezeRotation = false;
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.angularVelocity = 0;
         _jointWithPlayer.enabled = false;
     }
 }
