45b7a5f0599f664f6f78c997c02f56bd7752706c
HappyFaceFriend <yjl980730@gmail.com>
2024-01-26 15:47:37 +0900

    staystill 인풋세터에서 키 설정 변경 가능

--- Assets/Scripts/InputManager/InputSetters/StayStillInputSetter.cs
+++ Assets/Scripts/InputManager/InputSetters/StayStillInputSetter.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Stay Still InputSetter", menuName = "InputSetters/Stay Still")]
 public class StayStillInputSetter : InputSetterScriptableObject
 {
+    [SerializeField] KeyCode _dialogueKey;
     public override InputState GetState()
     {
         InputState state = new InputState();
         state.Movement = Vector2.zero;
         if (DialogueController.Instance.IsDialogueActive)
-            state.InteractionKey.Update(KeyCode.E);
+            state.InteractionKey.Update(_dialogueKey);
         return state;
     }
 
