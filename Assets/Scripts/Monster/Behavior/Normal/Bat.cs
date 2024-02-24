ef08116c6d483acaf07eec0ade90ca261aa00014
Junyeol Gong <gongjunyeol@gmail.com>
2024-02-25 05:03:29 +0900

    박쥐 점액 뿌리기 비주얼 작업

--- Assets/Scripts/Monster/Behavior/Normal/Bat.cs
+++ Assets/Scripts/Monster/Behavior/Normal/Bat.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bat : MonsterBehavior
@@ -12,7 +13,9 @@ public class Bat : MonsterBehavior
     [SerializeField] private Sprite[] _skillSprites;
 
     [SerializeField] private int _particleCount = 8;
-    [SerializeField] private float _shootingPower = 6f;
+    [SerializeField] private float _shootingPowerMin = 3f;
+    [SerializeField] private float _shootingPowerMax = 10f;
+    [SerializeField] private float _shootingPower = 10f;
     [SerializeField] private float _shootingAngle = 90f;
     [SerializeField] private float _shootingVariant = 40f;
 
@@ -47,11 +50,41 @@ public class Bat : MonsterBehavior
         for (int i = 0; i < _particleCount; i++)
         {
             Bat_Sprinkle particle = Instantiate(_batSkillPrefab, _shootPosition.position, Quaternion.identity);
+
             particle.SetSprite(_skillSprites[i % (_skillSprites.Length)]);
-            float angle = (i % 2 == 0) ? _shootingAngle : -_shootingAngle;
-            particle.Shoot(Random.Range(-_shootingVariant, _shootingVariant) + angle, _shootingPower);
+
+            // float angle = (i % 2 == 0) ? (180f - _shootingAngle) : _shootingAngle;
+            float angle = Random.Range((180f - _shootingAngle), _shootingAngle);
+            float finalAngle = angle + Random.Range(-_shootingVariant, _shootingVariant);
+            Vector3 dir = new Vector3(Mathf.Cos(finalAngle * Mathf.Deg2Rad), Mathf.Sin(finalAngle * Mathf.Deg2Rad), 0f)
+                    .normalized;
+            _shootingPower = Random.Range(_shootingPowerMin, _shootingPowerMax);
+            Vector3 force = dir * _shootingPower;
+
+            particle.Shoot(force);
         }
 
         GetComponent<SoundList>().PlaySFX("SE_Bat");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 vector1 = new Vector3(Mathf.Cos(_shootingAngle * Mathf.Deg2Rad), Mathf.Sin(_shootingAngle * Mathf.Deg2Rad), 0f).normalized;
+        Vector3 vector2 = new Vector3(Mathf.Cos((180f - _shootingAngle) * Mathf.Deg2Rad), Mathf.Sin((180f - _shootingAngle) * Mathf.Deg2Rad), 0f).normalized;
+        Vector3 vector1_1 = new Vector3(Mathf.Cos((_shootingAngle + _shootingVariant) * Mathf.Deg2Rad), Mathf.Sin((_shootingAngle + _shootingVariant) * Mathf.Deg2Rad), 0f).normalized;
+        Vector3 vector1_2 = new Vector3(Mathf.Cos((_shootingAngle - _shootingVariant) * Mathf.Deg2Rad), Mathf.Sin((_shootingAngle - _shootingVariant) * Mathf.Deg2Rad), 0f).normalized;
+        Vector3 vector2_1 = new Vector3(Mathf.Cos(((180f - _shootingAngle) + _shootingVariant) * Mathf.Deg2Rad), Mathf.Sin(((180f - _shootingAngle) + _shootingVariant) * Mathf.Deg2Rad), 0f).normalized;
+        Vector3 vector2_2 = new Vector3(Mathf.Cos(((180f - _shootingAngle) - _shootingVariant) * Mathf.Deg2Rad), Mathf.Sin(((180f - _shootingAngle) - _shootingVariant) * Mathf.Deg2Rad), 0f).normalized;
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector1 * _shootingPower);
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector2 * _shootingPower);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector1_1 * _shootingPower);
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector1_2 * _shootingPower);
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector2_1 * _shootingPower);
+        Gizmos.DrawLine(_shootPosition.position, _shootPosition.position + vector2_2 * _shootingPower);
+
     }
 }
