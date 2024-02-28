ca7ebe405d3138635ecbfdccd8ab1747ed7ced0d
Junyeol Gong <gongjunyeol@gmail.com>
2024-02-28 18:47:02 +0900

    키(Key) 튜토리얼 툴 제작

--- Assets/Scripts/MapObjects/TutorialZone.cs
+++ Assets/Scripts/MapObjects/TutorialZone.cs
@@ -1,43 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TutorialZone : TriggerZone
 {
-    [SerializeField] SpriteRenderer _tutorialImage;
+    [SerializeField] GameObject skillObject;
     [SerializeField] float _fadeInDuration = 0.3f;
     [SerializeField] float _fadeOffDuration = 0.3f;
-    float _originalAlpha = 1f;
+
+    private float _originalAlpha = 1f;
+
+    public Image[] images;
+    public TextMeshProUGUI[] textMeshs;
 
     private void Awake()
     {
-        _originalAlpha = _tutorialImage.color.a;
-        Color color = _tutorialImage.color;
-        color.a = 0;
-        _tutorialImage.color = color;
+        images = skillObject.GetComponentsInChildren<Image>();
+        textMeshs = skillObject.GetComponentsInChildren<TextMeshProUGUI>();
 
+        // √ ±‚»≠
+        foreach (var image in images)
+        {
+            Color tempColor = image.color;
+            tempColor.a = 0f;
+            image.color = tempColor;
+        }
+        foreach (var textMesh in textMeshs)
+        {
+            Color tempColor = textMesh.color;
+            tempColor.a = 0f;
+            textMesh.color = tempColor;
+        }
     }
     public override void OnPlayerEnter(PlayerBehaviour player)
     {
-        StartCoroutine(FadeCoroutine(0, _originalAlpha, _fadeInDuration));
+        StartCoroutine(FadeAllCoroutine(0f, _originalAlpha, _fadeInDuration));
     }
     public override void OnPlayerExit(PlayerBehaviour player)
     {
-        StartCoroutine(FadeCoroutine( _originalAlpha, 0, _fadeOffDuration));
+        StartCoroutine(FadeAllCoroutine(_originalAlpha, 0f, _fadeOffDuration));
     }
-    IEnumerator FadeCoroutine(float from, float to, float duration)
+    IEnumerator FadeAllCoroutine(float from, float to, float duration)
     {
         float eTime = 0f;
-        Color color = _tutorialImage.color;
         while (eTime < duration)
         {
-            color.a = Mathf.Lerp(from, to, eTime / duration);
-            _tutorialImage.color = color;
-            yield return null;
-            eTime += Time.deltaTime;
-        }
-        color.a = to;
-        _tutorialImage.color = color;
+            Color[] tempColors = new Color[images.Length + textMeshs.Length];
 
+            for (int i = 0; i < images.Length; i++)
+            {
+                float alpha = Mathf.Lerp(from, to, eTime / duration);
+                tempColors[i] = images[i].color;
+                tempColors[i].a = alpha;
+                images[i].color = tempColors[i];
+            }
+            for (int i = 0; i < textMeshs.Length; i++)
+            {
+                float alpha = Mathf.Lerp(from, to, eTime / duration);
+                tempColors[images.Length + i] = textMeshs[i].color;
+                tempColors[images.Length + i].a = alpha;
+                textMeshs[i].color = tempColors[images.Length + i];
+            }
+
+            eTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
