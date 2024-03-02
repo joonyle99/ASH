a33d4867612a23ae9243787d065d34b599fac6b1
Junyeol Gong <gongjunyeol@gmail.com>
2024-03-03 04:57:58 +0900

    플레이어 NPC와의 대화 후 애니메이션 재생 안되는 버그 수정

--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class InteractionController : MonoBehaviour
 {
-    // 범위 안의 상호작용한 오브젝트 리스트
+    // 범위 안의 상호작용 가능한 오브젝트 후보 리스트
     List<InteractableObject> _interactablesInRange = new List<InteractableObject>();
+
     InteractionMarker _interactionMarker;
+    [SerializeField] InteractableObject _interactionTarget;
+
     PlayerBehaviour _player;
-    [SerializeField] InteractableObject _interactionTarget = null;
 
     public InteractableObject InteractionTarget { get { return _interactionTarget; } }
-    bool _shouldDetectInteractable { get { return _interactionTarget == null || !_interactionTarget.IsInteracting; } }
+    bool _shouldDetectInteractable { get { return _interactionTarget == null || !_interactionTarget.IsInteracting; } }      // 상호작용 대상을 찾는 프로세스를 진행해야 하는 상태
 
     private void Awake()
     {
@@ -41,13 +43,14 @@ public class InteractionController : MonoBehaviour
 
         _interactionTarget = newTarget;
 
-        if(_interactionTarget == null)
+        if (_interactionTarget == null)
             _interactionMarker.Disable();
         else
             _interactionMarker.EnableAt(newTarget);
     }
-    void OnInteractionStart()
+    public void OnInteractionStart()
     {
+        // 모든 상호작용이 Interaction State로 전이돼야 하는 것은 아니다 (다이얼로그도 상호작용임)
         if (_interactionTarget.StateChange == InteractionStateChangeType.InteractionState)
             _player.ChangeState<InteractionState>();
     }
@@ -66,6 +69,7 @@ public class InteractionController : MonoBehaviour
 
         if (_interactionTarget != null)
         {
+            // 상호작용 키를 입력받은 경우
             if (InputManager.Instance.State.InteractionKey.KeyDown)
             {
                 // 상호작용 가능한 State
@@ -96,14 +100,18 @@ public class InteractionController : MonoBehaviour
 
     void SetTargetToClosestInteractable()
     {
+        // 리스트의 null을 제거하며 정리한다 (x == null인 경우 모든 x를 제거 *람다식*)
         _interactablesInRange.RemoveAll(x => x == null);
 
         float minDist = float.MaxValue;
         int minIndex = -1;
         for (int i = 0; i < _interactablesInRange.Count; i++)
         {
+            // 상호작용 기능 활성화 여부 판단
             if (!_interactablesInRange[i].IsInteractable)
                 continue;
+
+            // 제곱근 연산은 뉴튼 랩슨 알고리즘을 사용하며 계산 비용이 크기 때문에 제곱의 형태로 반환 (비교에는 문제가 없다)
             float dist = Vector3.SqrMagnitude(_interactablesInRange[i].transform.position - transform.position);
             if (dist < minDist)
             {
--- Assets/Scripts/Player/States/InteractionState.cs
+++ Assets/Scripts/Player/States/InteractionState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 /// <summary>
 /// 플레이어가 상호작용 하는 경우에 진입하는 상태
 /// Enum 정보를 가지고 애니메이션 및 동작을 적용한다
@@ -13,7 +15,8 @@ public class InteractionState : PlayerState
                 Player.Animator.SetBool("IsPush", true);
                 break;
         }
-        Player.Animator.SetTrigger("Interact");
+        if (Player.InteractionController.InteractionTarget.AnimationType != InteractionAnimationType.None)
+            Player.Animator.SetTrigger("Interact");
     }
 
     protected override void OnUpdate()
