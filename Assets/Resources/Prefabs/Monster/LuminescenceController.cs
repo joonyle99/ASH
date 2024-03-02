74c62f7ea5c43eb62c69a361537ab6988d8084d3
Junyeol Gong <gongjunyeol@gmail.com>
2024-03-03 04:31:51 +0900

    루미네센스 빛나는 기능 설정값 수정

--- Assets/Resources/Prefabs/Monster/LuminescenceController.cs
+++ Assets/Resources/Prefabs/Monster/LuminescenceController.cs
@@ -14,7 +14,7 @@ public class LuminescenceController : MonoBehaviour
 
     void Update()
     {
-        // luminescence의 intensity를 0.3 ~ 0.6을 계속 반복하도록
-        luminescence.intensity = (Mathf.Sin(Time.time * speed) * 0.15f) + 0.45f;
+        // luminescence의 intensity를 0.3 ~ 0.8을 계속 반복하도록
+        luminescence.intensity = (Mathf.Sin(Time.time * speed) * 0.25f) + 0.55f;
     }
 }
