cc765e412d16e032fc3dbc9ca29ac422cec3d9c0
Junyeol Gong <gongjunyeol@gmail.com>
2024-05-16 04:38:51 +0900

    Revert "Title Scene에서 Build 시 URP Shader 깨지는 버그 찾는 중"
    This reverts commit a2bd795365a992c94acb69483e5261940789f68f.

--- Assets/Scripts/UIs/TitleScene/TitleSceneManager.cs
+++ Assets/Scripts/UIs/TitleScene/TitleSceneManager.cs
@@ -3,31 +3,36 @@ using UnityEngine;
 
 public class TitleSceneManager : MonoBehaviour
 {
+    //[SerializeField] PassageData _initialPassageData;
+
     private void Start()
     {
         SoundManager.Instance.PlayCommonBGM("MainTheme");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        StartCoroutine(SceneChangeCoroutine(sceneName));
     }
 
     public void OpenPrologueScene()
     {
         StartCoroutine(OpenPrologueSceneCoroutine());
     }
-    private IEnumerator OpenPrologueSceneCoroutine()
+
+    IEnumerator OpenPrologueSceneCoroutine()
     {
         yield return SceneContext.Current.SceneTransitionPlayer.ExitSceneEffectCoroutine();
         SceneChangeManager.Instance.ChangeToScene("PrologueScene");
     }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-    public void ChangeScene(string sceneName)
-    {
-        StartCoroutine(SceneChangeCoroutine(sceneName));
-    }
-    private IEnumerator SceneChangeCoroutine(string sceneName)
+    IEnumerator SceneChangeCoroutine(string sceneName)
     {
         yield return SceneContext.Current.SceneTransitionPlayer.ExitSceneEffectCoroutine();
-        SceneChangeManager.Instance.ChangeToScene(sceneName, () => SoundManager.Instance.PlayCommonBGM("Exploration1"));
+        SceneChangeManager.Instance.ChangeToScene(sceneName, ()=>  SoundManager.Instance.PlayCommonBGM("Exploration1"));
     }
 }
