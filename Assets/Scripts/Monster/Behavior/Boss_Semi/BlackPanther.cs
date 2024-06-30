9e2e40868bedfe58d258f5feca4cdbf334cfd60d
Junyeol Gong <gongjunyeol@gmail.com>
2024-07-01 02:15:10 +0900

    ground black line material

--- Assets/Scripts/Monster/Behavior/Boss_Semi/BlackPanther.cs
+++ Assets/Scripts/Monster/Behavior/Boss_Semi/BlackPanther.cs
@@ -1,7 +1,5 @@
-using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public sealed class BlackPanther : BossBehaviour, ILightCaptureListener
