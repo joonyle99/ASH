847bc4a613e350707bcb51b5287b45814f6e3e1a
Junyeol Gong <gongjunyeol@gmail.com>
2024-07-13 03:10:02 +0900

    여러 수정 사항
    * 2-1 돌 상호작용 끄기
    * URP asset 재적용

--- Assets/TriggerEnter2DInvoker.cs
+++ Assets/TriggerEnter2DInvoker.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 /// 런타임에 타입 정보에 접근하고 조작할 수 있는 기능을 제공
 /// 
 /// ex) 리플렉션 사용 예시
-/// 특정 플러그인을 동적으로 로드할 때 사용
-/// 비공개 멤버에 접근하여 내부 상태를 검사하거나 테스트
-/// ORM(Object-Relational Mapping): 데이터베이스 엔티티와 객체 간의 매핑을 동적으로 처리할 때 사용
-/// 객체를 동적으로 직렬화하거나 역직렬화할 때 사용
+/// * 특정 플러그인을 동적으로 로드할 때 사용
+/// * 비공개 멤버에 접근하여 내부 상태를 검사하거나 테스트
+/// * ORM(Object-Relational Mapping): 데이터베이스 엔티티와 객체 간의 매핑을 동적으로 처리할 때 사용
+/// * 객체를 동적으로 직렬화하거나 역직렬화할 때 사용
 
+/// <summary>
+/// 대상의 OnTriggerEnter2D 메서드를 리플렉션을 통해 호출하는 컴포넌트
+/// </summary>
 public class TriggerEnter2DInvoker : MonoBehaviour
 {
     [SerializeField] private MonoBehaviour _targetBehaviour;
