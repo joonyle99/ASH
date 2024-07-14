a8895349074ce60ec98dbfa349ea787838ef1d67
Junyeol Gong <gongjunyeol@gmail.com>
2024-07-14 21:13:36 +0900

    챕터 1 몬스터 프리팹 정리 및 버섯 Devour 컷씬 로직 삭제

--- Assets/Scripts/Monster/Skill/Mushroom_Devour.cs
+++ Assets/Scripts/Monster/Skill/Mushroom_Devour.cs
@@ -1,34 +1,4 @@
-using UnityEngine;
-
 public class Mushroom_Devour : Monster_BodySkill
 {
-    [SerializeField]
-    private CutscenePlayerList _cutscenePlayerList;
 
-    protected override void Awake()
-    {
-        base.Awake();
-
-        if (_cutscenePlayerList)
-        {
-            if (_cutscenePlayerList.FindCutscene("Lighting Guide"))
-            {
-                Debug.Log($"{name} 인스턴스에서 LightCutscene을 등록합니다");
-
-                monsterSkillCutScene -= LightCutscene;
-                monsterSkillCutScene += LightCutscene;
-            }
-        }
-    }
-
-    private void LightCutscene()
-    {
-        Debug.Log("LightCutscene");
-
-        // 컷씬을 재생
-        _cutscenePlayerList.PlayCutscene("Lighting Guide");
-
-        // 이벤트를 한 번만 호출하도록 제거
-        monsterSkillCutScene -= LightCutscene;
-    }
 }
