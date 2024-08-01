1e0e0b873970735aa29bb631babd612d9457cd19
Junyeol Gong <gongjunyeol@gmail.com>
2024-08-02 00:37:32 +0900

    루미네센스 폴리싱

--- Assets/Scripts/Monster/_ETC/LuminescenceController.cs
+++ Assets/Scripts/Monster/_ETC/LuminescenceController.cs
@@ -6,14 +6,17 @@ public class LuminescenceController : MonoBehaviour
     public Light2D luminescence;
     public float speed;
 
+    private float _startIntensity;
+
     void Awake()
     {
         luminescence = GetComponent<Light2D>();
+        _startIntensity = luminescence.intensity;
     }
 
     void Update()
     {
-        // luminescence의 intensity를 0.3 ~ 0.8을 계속 반복하도록
-        luminescence.intensity = (Mathf.Sin(Time.time * speed) * 0.25f) + 0.55f;
+        // luminescence의 intensity를 _startIntensity + (-0.5 ~ 0.5) 를 계속 반복하도록
+        luminescence.intensity = _startIntensity + (Mathf.Sin(Time.time * speed) * 0.5f);
     }
 }
