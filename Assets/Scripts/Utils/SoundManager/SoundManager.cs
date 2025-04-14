91ad252355ace66fdef9854229df3227c0df9229
joonyle99 <gongjunyeol@gmail.com>
2025-04-14 23:26:46 +0900

    까마귀 배경음 제거

--- Assets/Scripts/Utils/SoundManager/SoundManager.cs
+++ Assets/Scripts/Utils/SoundManager/SoundManager.cs
@@ -351,9 +351,11 @@ public class SoundManager : HappyTools.SingletonBehaviourFixed<SoundManager>, IS
         else
         {
             //BGMFadeInOut("BasicBGM", 5, "Dungeon_Wave", 5);
-            // 기본 BGM 재생
+
             StopBGM();
-            PlayCommonBGM("BasicBGM");
+
+            // 기본 BGM 재생
+            //PlayCommonBGM("BasicBGM");
         }
     }
 
